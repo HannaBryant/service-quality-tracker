314f4eb4eb1215ae1a711b0fa9a4d1ab61a98be7
Hanna Bryant <hannab329@gmail.com>
2025-11-26 02:08:41 +0000

    completed feedback comments

--- serviceTracker.js
+++ serviceTracker.js
@@ -3,14 +3,10 @@ let input = require ('prompt-sync')();
 let feedbackComments =[];
 let satisfactionRatings = [];
 
-let i = 0;
-
-while (i < 5) {
+for (let i = 0; i < 5; i = i + 1){
     feedbackComments.push(input("Enter feedback comment:"));
-    satisfactionRatings.push(Number(input("Enter rating (1-5):")));
-
-    i = i + 1
+    satisfactionRatings.push(parseFloat(input ("Enter rating (1-5): ")));
 }
 
-console.table(feedbackComments);
-console.table(satisfactionRatings);
+            console.table(feedbackComments);
+            console.table(satisfactionRatings);
